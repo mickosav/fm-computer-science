ddfa9edd9e997e027c7345fce6ab0f9413e8fd23
Milutin Savovic <mickosavovic@gmail.com>
2019-05-06 17:03:01 +0200

    Quick sort

--- sorting-algos.js
+++ sorting-algos.js
@@ -10,18 +10,18 @@ You have to have a base case and you want it very well handled.
 */
 
 // factorial n * (n-1) * (n-2) * ... 3, 2, 1
-function factorial (n) {
-  if (n <= 1) {
-    return 1
-  }
+function factorial(n) {
+	if (n <= 1) {
+		return 1
+	}
 
-  return n * factorial(n - 1)
+	return n * factorial(n - 1)
 }
 
-function fibonacci (n) {
-  if (n <= 2) return 1
+function fibonacci(n) {
+	if (n <= 2) return 1
 
-  return fibonacci(n - 1) + fibonacci(n - 2)
+	return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 /*
@@ -32,19 +32,19 @@ swapp their places if the smaller index's value
 is larger than the larger index's value
 */
 
-function bubbleSort (arr) {
-  let swapped
-  do {
-    swapped = false
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
-        swapped = true
-      }
-    }
-  } while (swapped)
+function bubbleSort(arr) {
+	let swapped
+	do {
+		swapped = false
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] > arr[i + 1]) {
+				;[ arr[i], arr[i + 1] ] = [ arr[i + 1], arr[i] ]
+				swapped = true
+			}
+		}
+	} while (swapped)
 
-  return arr
+	return arr
 }
 
 /*
@@ -56,23 +56,22 @@ The outer loop goes over the whole list, the index of which signifies where the 
 loop goes over the sorted part of the list and inserts first item after sorted part into the correct position.
 */
 
-
 // [4, 2, 3, 1, 5]
 // [2, 4, 3, 1, 5]
 // [2, 3, 4, 1, 5]
 // [2, 3, 4, 5]
 // [2, 3, 4, 5]
-function insertionSort (arr) {
-  for (let i = 0, len = arr.length; i < len - 1; i++) {
-    for (let k = 0; k <= i; k++) {
-      if (arr[i + 1] < arr[k]) {
-        const spliced = arr.splice(i + 1, 1)[0]
-        arr.splice(k, 0, spliced)
+function insertionSort(arr) {
+	for (let i = 0, len = arr.length; i < len - 1; i++) {
+		for (let k = 0; k <= i; k++) {
+			if (arr[i + 1] < arr[k]) {
+				const spliced = arr.splice(i + 1, 1)[0]
+				arr.splice(k, 0, spliced)
 
-        break
-      }
-    }
-  }
+				break
+			}
+		}
+	}
 }
 
 /*
@@ -82,32 +81,55 @@ and recursively call merge sort on those smaller list, which in turn will do the
 The base case is when you have a list of one, at which point you will return that sorted list of one.
 */
 
-function mergeSort (arr) {
-  const length = arr.length
-  if (length <= 1) return arr
+function mergeSort(arr) {
+	const length = arr.length
+	if (length <= 1) return arr
 
-  const middle = Math.round(length / 2)
-  const left = arr.slice(0, middle)
-  const right = arr.slice(middle)
+	const middle = Math.round(length / 2)
+	const left = arr.slice(0, middle)
+	const right = arr.slice(middle)
 
-  const sortedLeft = mergeSort(left)
-  const sortedRight = mergeSort(right)
+	const sortedLeft = mergeSort(left)
+	const sortedRight = mergeSort(right)
 
-  return merge(sortedLeft, sortedRight)
+	return merge(sortedLeft, sortedRight)
 }
 
 function merge(left, right) {
-  const results = []
+	const results = []
 
-  while(left.length && right.length) {
-    if (left[0] <= right[0]) {
-      results.push(left.shift())
-    } else {
-      results.push(right.shift())
-    }
-  }
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			results.push(left.shift())
+		} else {
+			results.push(right.shift())
+		}
+	}
 
-  // one of the arrays is empty, but we don't care which one
-  // we want to add the one that isn't to results arr
-  return results.concat(left, right)
+	// one of the arrays is empty, but we don't care which one
+	// we want to add the one that isn't to results arr
+	return [ ...results, ...left, ...right ]
+}
+
+/*  Exercise 5: Quick sort - very useful
+The basic gist is that you take the last element in the list and call that the pivot.
+Everything that's smaller than the pivot gets put into a "left" list and everything that's greater get's put in a "right" list.
+You then call quick sort on the left and right lists independently (hence the recursion.)
+After those two sorts come back, you concatenate the sorted left list, the pivot,
+and then the right list (in that order.) The base case is when you have a list of length 1 or 0,
+where you just return the list given to you.
+*/
+
+function quickSort (arr) {
+	const length = arr.length
+	if (length <= 1) return arr
+
+	const pivot = arr.pop()
+	const lessOrEqual = []
+	const bigger = []
+
+	arr.forEach(n => n < pivot ? lessOrEqual.push(n) : bigger.push(n))
+
+
+	return [ ...quickSort(lessOrEqual), pivot, ...quickSort(bigger) ]
 }
